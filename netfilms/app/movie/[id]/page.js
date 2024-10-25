6e110324a69cd7d1079b1532687c5255bbafa5d5
SumeyraSeyma <114585202+SumeyraSeyma@users.noreply.github.com>
2024-10-25 17:39:29 +0300

    Loading pages and skeleton

--- netfilms/app/movie/[id]/page.js
+++ netfilms/app/movie/[id]/page.js
@@ -4,9 +4,15 @@ import React from "react";
 import  MovieContainer  from "@/containers/movie";
 import  Movies  from "@/mocks/movies.json";
 
-
+async function delay (ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 async function MoviePage({ params, searchParams }) {
+  await delay(1000);
+  
     console.log({Movies});
     const movieDetail = Movies.results.find((movie) => movie.id.toString() === params.id);
 
